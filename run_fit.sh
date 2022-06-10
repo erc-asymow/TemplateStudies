0308dcc88f305ad050af8b161cb968fe6df54c4c
Lorenzo <lore.bianchini@gmail.com>
2022-06-10 12:42:54 +0200

    fixed random generator

--- run_fit.sh
+++ run_fit.sh
@@ -4,7 +4,7 @@ GRID=$1
 
 for X in 12
 do
-    for Y in 5
+    for Y in 6
     do
 	for CORRX in 4
 	do
@@ -15,8 +15,12 @@ do
 		    for A0Y in 2
 		    do		    
 			echo 'Now doing ', $X, $Y, $CORRX, $CORRY
-			./fit --nevents=10000000 --tag=$GRID --run=closure --degs_pdf_x=$X --degs_pdf_y=$Y --degs_corr_x=$CORRX --degs_corr_y=$CORRY\
-		        --degs_A0_x=$A0X --degs_A0_y=$A0Y
+			./fit --nevents=-1 --tag=$GRID --run=closure --degs_pdf_x=$X --degs_pdf_y=$Y --degs_corr_x=$CORRX --degs_corr_y=$CORRY\
+		        --degs_A0_x=$A0X --degs_A0_y=$A0Y\
+                        --j0 --j2
+			#--j1 --j2
+                        #--j0
+                        #--j0
 		    done
 		done
 	    done
