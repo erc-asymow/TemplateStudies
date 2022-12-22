d96253391e22648f5f89a2b8781e8f68ba780e6d
Lorenzo <lore.bianchini@gmail.com>
2022-12-22 12:28:30 +0100

    before leaving

--- macros/fit_vsN.C
+++ macros/fit_vsN.C
@@ -1,35 +1,38 @@
 {
 
   bool logy = true;
-  float norm_lumi = (0.100/0.800);
-    
-  TCanvas* c = new TCanvas("c", "canvas", 1000, 600);
+  
+  float norm_lumi    = (0.100/0.800);
+  TString image_name = "deltaM_vs_N_paper2_corr.eps";
+  TString header     = "L = 1.0#times10^{8} events, p_{T}/GeV#in[25,60], |#eta|<2.5";
+
+  TCanvas* c = new TCanvas("c", "canvas", 1200, 800);
   c->SetGridy();
 
   TMultiGraph* mg = new TMultiGraph();
-  TLegend* leg1 = new TLegend(0.10, 0.62, 0.70, 0.90, "","brNDC");
+  TLegend* leg1 = new TLegend(0.10, 0.68, 0.70, 0.90, "","brNDC");
   leg1->SetFillStyle(0);
   leg1->SetBorderSize(0);
-  leg1->SetTextSize(0.025);
+  leg1->SetTextSize(0.035);
   leg1->SetFillColor(10);
-  leg1->SetNColumns(2);
-  leg1->SetHeader("Norm. = 1.0#times10^{8} events, p_{T}/GeV#in[25,60], |#eta|<2.5");
-
-  TString image_name = "deltaM_vs_N_paper2.png";
+  leg1->SetNColumns(4);
+  leg1->SetHeader(header);
 
   vector<TString> files_n;
   vector<TString> files_l;
-  files_n.emplace_back("UL_10_4_A0_3_2_A1_2_3_A2_3_2_A3_3_4_A4_3_3_full"); files_l.emplace_back("Cheb");
-  //files_n.emplace_back("UL_10_4_A0_2_2_A1_2_3_A2_2_2_A3_3_4_A4_3_3_full"); files_l.emplace_back("A^{(0,2)}_{x}: 3#rightarrow2");
-  //files_n.emplace_back("UL_10_4_A0_3_2_A1_3_3_A2_3_2_A3_2_4_A4_3_3_full"); files_l.emplace_back("A^{(3)}_{x}: 3#rightarrow2");
-  //files_n.emplace_back("UL_10_4_A0_3_2_A1_3_3_A2_3_2_A3_3_2_A4_3_3_full"); files_l.emplace_back("A^{(3)}_{y}: 4#rightarrow2");
-  //files_n.emplace_back("UL_10_4_A0_3_2_A1_3_3_A2_3_2_A3_3_4_A4_1_3_full"); files_l.emplace_back("A^{(4)}_{x}: 3#rightarrow1");
-  files_n.emplace_back("UL_3_2_A0_2_2_A1_2_2_A2_2_2_A3_2_2_A4_2_2_corr"); files_l.emplace_back("Corr");
-  //files_n.emplace_back("UL_5_2_A0_2_2_A1_2_2_A2_2_2_A3_2_2_A4_2_2_corr"); files_l.emplace_back("UL_{x}: 3#rightarrow5 ");
-  //files_n.emplace_back("UL_3_2_A0_2_2_A1_2_2_A2_2_2_A3_1_2_A4_2_2_corr"); files_l.emplace_back("A^{(3)}_{x}: 2#rightarrow1");
-  //files_n.emplace_back("UL_3_2_A0_2_2_A1_2_2_A2_2_2_A3_2_2_A4_1_2_corr"); files_l.emplace_back("A^{(3)}_{x}: 2#rightarrow1");
-  //files_n.emplace_back("UL_3_2_A0_1_2_A1_1_2_A2_1_2_A3_1_2_A4_1_2_corr"); files_l.emplace_back("A^{(k)}_{x}: 2#rightarrow");
-  files_n.emplace_back("UL_8_6_A0_8_6_A1_8_6_A2_8_6_A3_8_6_A4_8_6_grid"); files_l.emplace_back("grid");
+  vector<int>     files_c;
+  vector<float>   files_w;
+  //files_n.emplace_back("UL_8_6_A0_8_6_A1_8_6_A2_8_6_A3_8_6_A4_8_6_grid"); files_l.emplace_back("w_{grid}");                       files_c.emplace_back(1); files_w.emplace_back(3); 
+  //files_n.emplace_back("UL_10_4_A0_3_2_A1_2_3_A2_3_2_A3_3_4_A4_3_3_full"); files_l.emplace_back("w_{cheb}");                      files_c.emplace_back(kRed);   files_w.emplace_back(3); 
+  //files_n.emplace_back("UL_10_4_A0_2_2_A1_2_3_A2_2_2_A3_3_4_A4_3_3_full"); files_l.emplace_back("A^{(0,2)}_{x}: 3#rightarrow2"); files_c.emplace_back(kRed-4); files_w.emplace_back(1.5);
+  //files_n.emplace_back("UL_10_4_A0_3_2_A1_2_3_A2_3_2_A3_2_4_A4_3_3_full"); files_l.emplace_back("A^{(3)}_{x}: 3#rightarrow2");   files_c.emplace_back(kRed-7); files_w.emplace_back(1.5);
+  //files_n.emplace_back("UL_10_4_A0_3_2_A1_2_3_A2_3_2_A3_3_2_A4_3_3_full"); files_l.emplace_back("A^{(3)}_{y}: 4#rightarrow2");   files_c.emplace_back(kRed-9); files_w.emplace_back(1.5);
+  //files_n.emplace_back("UL_10_4_A0_3_2_A1_2_3_A2_3_2_A3_3_4_A4_1_3_full"); files_l.emplace_back("A^{(4)}_{x}: 3#rightarrow1");   files_c.emplace_back(kRed-10); files_w.emplace_back(1.5);
+  files_n.emplace_back("UL_3_2_A0_2_2_A1_2_2_A2_2_2_A3_2_2_A4_2_2_corr"); files_l.emplace_back("w_{corr}");                       files_c.emplace_back(kBlue);   files_w.emplace_back(3); 
+  files_n.emplace_back("UL_5_2_A0_2_2_A1_2_2_A2_2_2_A3_2_2_A4_2_2_corr"); files_l.emplace_back("UL_{x}: 3#rightarrow5 ");        files_c.emplace_back(kBlue-7); files_w.emplace_back(1.5);
+  files_n.emplace_back("UL_3_2_A0_2_2_A1_2_2_A2_2_2_A3_1_2_A4_2_2_corr"); files_l.emplace_back("A^{(3)}_{x}: 2#rightarrow1");    files_c.emplace_back(kBlue-9); files_w.emplace_back(1.5);
+  files_n.emplace_back("UL_3_2_A0_2_2_A1_2_2_A2_2_2_A3_2_2_A4_1_2_corr"); files_l.emplace_back("A^{(4)}_{x}: 2#rightarrow1");    files_c.emplace_back(kBlue-8); files_w.emplace_back(1.5);
+  files_n.emplace_back("UL_3_2_A0_1_2_A1_1_2_A2_1_2_A3_1_2_A4_1_2_corr"); files_l.emplace_back("A^{(k)}_{x}: 2#rightarrow1");    files_c.emplace_back(kBlue-5); files_w.emplace_back(1.5);
   
   vector<TString> posttags = {
     "jUL_j0_j1_j2_j3_j4_DEBUG_ADDMC_ULA0A1A2A3A4",
@@ -82,26 +85,33 @@
       fit_nevents->SetLineStyle(kSolid);
       fit_nevents->SetMarkerStyle(kFullCircle);
     }
-    fit_nevents->SetMarkerSize(1.5);
-    fit_nevents->SetMarkerColor(1);
-    fit_nevents->SetLineColor(1);
+    if(files_w[i2]>2)
+      fit_nevents->SetMarkerSize(1.5);
+    else
+      fit_nevents->SetMarkerSize(1.0);
+      
+    fit_nevents->SetMarkerColor(files_c[i2]);
+    fit_nevents->SetLineColor(files_c[i2]);
+    fit_nevents->SetLineWidth(files_w[i2]);
     fit_nevents->SetMinimum(0.0);
     fit_nevents->SetMaximum(55.0);
     TString leg_text = files_l[i2];
-    if(i1==0) leg_text += " (w/ MC stat)";
-    leg1->AddEntry(fit_nevents, leg_text, "PL");
+    if(i1==0){
+      leg_text += " (w/ MC stat)";
+    }
+    if(i1==1) leg1->AddEntry(fit_nevents, leg_text, "PL");
     mg->Add(fit_nevents);
     }
   }
   ////////////////////////////////////////////////////////
   mg->GetXaxis()->SetTitle("N_{tot}");
-  mg->GetXaxis()->SetTitleSize(0.05);
-  mg->GetXaxis()->SetTitleOffset(0.8);
+  mg->GetXaxis()->SetTitleSize(0.045);
+  mg->GetXaxis()->SetTitleOffset(0.9);
   mg->GetYaxis()->SetTitleSize(0.06);
   mg->GetYaxis()->SetTitleOffset(0.7);
   mg->GetYaxis()->SetTitle("#Delta M_{W} (MeV)");
-  if(logy) mg->GetXaxis()->SetTitle("log_{10}(N_{unweigh.}/N_{tot})");
-  mg->GetYaxis()->SetRangeUser(4, 1000.);
+  if(logy) mg->GetXaxis()->SetTitle("log_{10}(N/L)");
+  mg->GetYaxis()->SetRangeUser(4, 500.);
   mg->GetXaxis()->SetRangeUser(0.0001, 100e+09);
   if(logy) mg->GetXaxis()->SetRangeUser(-4, 5);
 
@@ -111,7 +121,7 @@
   mg->Draw("apl");
   c->SetLogy();
   leg1->Draw();
-  //c->SaveAs(image_name);
+  c->SaveAs(image_name);
   //c->SaveAs("deltaM_vs_y_finalfix.png");
 
   //TF1* func = new TF1("func", "[0]*TMath::Log(x)+[1]", 0.0001, 50e+09);
@@ -119,3 +129,4 @@
 
   return;
 }
+
