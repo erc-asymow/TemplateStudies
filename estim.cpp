5578c34c8fe13127f02d59533fa5c1f6732ba5f3
Cristina-Andreea Alexe <alexe.cristina99@gmail.com>
2023-01-30 17:15:40 +0100

    corrected histogram

--- estim.cpp
+++ estim.cpp
@@ -1,3 +1,4 @@
+
 #include <ROOT/RDataFrame.hxx>
 #include "TFile.h"
 #include "TRandom3.h"
@@ -6,6 +7,7 @@
 #include "TMath.h"
 #include "TF1.h"
 #include "TF2.h"
+#include "TStyle.h"
 #include "TCanvas.h"
 #include <TMatrixD.h>
 #include <TStopwatch.h>
@@ -39,24 +41,64 @@ int main()
   wxy->SetParameter(2, 1.0);
   wxy->SetParameter(3, 1.0);
   wxy->SetParameter(4, sigma2_y);
-  wxy->SetParameter(5, 0.0);  
+  wxy->SetParameter(5, 0.0);
+
+  TF2* wxytry = new TF2("wxytry","x+y", 0.0, max_x, -max_y, max_y);
   
-  TH2D* hist = new TH2D("hist","weights", n_bins_x, 0., max_x, n_bins_y, -max_y, max_y);
-  for(int i=0; i<=n_bins_x; i++){ //<= or < 
-    for(int k=0; k<=n_bins_y; k++){
+  TH2D* hist_notnorm = new TH2D("hist_notnorm","weights", n_bins_x, 0., max_x, n_bins_y, -max_y, max_y);
+  for(int i=1; i<=n_bins_x; i++){  
+    for(int k=1; k<=n_bins_y; k++){
       double x_bin_width = max_x/n_bins_x;
       double y_bin_width = 2*max_y/n_bins_y;
-      double weight=wxy->Eval(i*x_bin_width,-max_y + k*y_bin_width); //at what value should the fct be eval?
-      hist->Fill(i*x_bin_width, -max_y + k*y_bin_width, weight);
+      double weight=wxy->Eval((i-0.5)*x_bin_width, -max_y + (k-0.5)*y_bin_width); //at what value should the fct be eval?
+      hist_notnorm->Fill((i-0.5)*x_bin_width, -max_y + (k-0.5)*y_bin_width, weight);  
     }    
   }
 
-  double integral = hist->Integral("width");
+  /*
+  
+  for(int i=1; i<=n_bins_x; i++){
+    for(int k=1; k<=n_bins_y; k++){
+      double x_bin_width = max_x/n_bins_x;
+      double y_bin_width = 2*max_y/n_bins_y;
+      double weight=wxy->Eval((i-0.5)*x_bin_width, -max_y + (k-0.5)*y_bin_width); //at what value should the fct be eval?
+      std::cout<<"For: i="<<i<<" k="<<k<<" w="<<weight<<" hist get value="<<hist->GetBinContent(i,k)<<"for  x>"<<(i-0.5)*x_bin_width<<" y>"<<-max_y + (k-0.5)*y_bin_width<<"\n";
+    }
+  }
+
+  */
+  
+  double integral = hist_notnorm->Integral("width");
   std::cout<<"integral with width is: "<<integral;
+
+  TH2D* hist = new TH2D("hist","weights", n_bins_x, 0., max_x, n_bins_y, -max_y, max_y);
+  for(int i=1; i<=n_bins_x; i++){
+    for(int k=1; k<=n_bins_y; k++){
+      double x_bin_width = max_x/n_bins_x;
+      double y_bin_width = 2*max_y/n_bins_y;
+      double weight=wxy->Eval((i-0.5)*x_bin_width, -max_y + (k-0.5)*y_bin_width); //at what value should the fct be eval?
+      hist->Fill((i-0.5)*x_bin_width, -max_y + (k-0.5)*y_bin_width, weight/integral);
+    }
+  }
+
+//    /* To see the values of the bins
+
+  for(int i=1; i<=n_bins_x; i++){
+    for(int k=1; k<=n_bins_y; k++){
+      double x_bin_width = max_x/n_bins_x;
+      double y_bin_width = 2*max_y/n_bins_y;
+      double weight=wxy->Eval((i-0.5)*x_bin_width, -max_y + (k-0.5)*y_bin_width); //at what value should the fct be eval?
+      std::cout<<"For: i="<<i<<" k="<<k<<" w="<<weight<<" hist get value="<<hist->GetBinContent(i,k)<<"for  x>"<<(i-0.5)*x_bin_width<<" y>"<<-max_y + (k-0.5)*y_bin_width<<"\n";
+    }
+  }
+
+//  */
   
   TCanvas *c1 = new TCanvas("c1", "weights", 600, 600);
   c1->cd();
-  hist->Draw();
+  //gStyle->SetPalette(87);
+  gStyle->SetNumberContours(256);
+  hist->Draw("COLZ");
   c1->SaveAs("hist.pdf");
 }
 
