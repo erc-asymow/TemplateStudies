a05202040fb20543ec108267b987ad910ec7f350
Lorenzo <lore.bianchini@gmail.com>
2023-09-26 11:16:29 +0200

    more opts

--- run_getparam.py
+++ run_getparam.py
@@ -131,7 +131,8 @@ procs = {
      },
 }
 
-allowed_procs = ["A1","A2","A3","A4"]
+#allowed_procs = ["A1","A2","A3","A4"]
+allowed_procs = ["A2"]
 
 def plot_pvals(fnames=[], metric="pvals", proc="wp"):
     ROOT.gStyle.SetPadRightMargin(0.15)
