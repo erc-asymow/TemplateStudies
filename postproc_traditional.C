ca32937e78b7fe3c681061abe4ace522443941b6
Lorenzo <lore.bianchini@gmail.com>
2024-05-13 12:07:26 +0200

    more options

--- postproc_traditional.C
+++ postproc_traditional.C
@@ -19,16 +19,24 @@
   scales_names.emplace_back("muRDown");
   scales.emplace_back( std::make_pair<TString,int>("twoone", 5) );
   scales_names.emplace_back("muRUp");
-    
-  vector<TString> procs = {"A0",
-			   "A1", "A2", "A3", "A4"
-  };
+
   
-  TFile* fout = TFile::Open("fout_systTrad_"+opt+"_"+input_tag+".root", "RECREATE");
+  vector<TString> procs = {"A0",
+			   "A1",
+			   "A2",
+			   "A3",
+			   "A4"
+  };
 
+  TFile* fout = TFile::Open("fout_systTrad_"+opt+"_"+phase_space+"_"+input_tag+".root", "RECREATE");
+  cout << fout->GetName() <<  " created" << endl;
+  
   TFile* fin = TFile::Open("root/fout_syst_"+opt+"_"+phase_space+"_"+input_tag+".root", "READ");
-
+  cout << fin->GetName() << " opened" << endl;
+    
   TH2D* h_syst_i = 0;
+  TH2D* h_systenvUp_i = 0;
+  TH2D* h_systenvDown_i = 0;
 
   // loop over procs
   for(unsigned int iproc = 0; iproc<procs.size(); iproc++){
@@ -51,22 +59,24 @@
     h_pdf->Write();
 
     for(int isyst=0; isyst<nfp; isyst++ ){
-      cout << "Syst " << isyst << endl;
+      cout << "\tSyst " << isyst << endl;
       
       TH1D* h_node_i = (TH1D*)fin->Get(proc+"/h_nodes_"+proc+"_syst"+TString(Form("%d",isyst)));
       double node_x = h_node_i->GetBinContent(1);
       double node_y = TMath::Abs(h_node_i->GetBinContent(2));
       if(node_y>=yf_max) node_y -= 1e-03;
       if(node_x>=xf_max) node_x -= 1e-03;
-      cout << node_x << ", " << node_y << endl;
-      TH2D* h_syst_in_input = (TH2D*)fin->Get(proc+"/h_pdf_"+proc+"_syst"+TString(Form("%d",isyst))+"Up");
+      cout << "\tNodes: (" <<  node_x << ", " << node_y << ")" << endl;
+      TH2D* h_syst_in_input = (TH2D*)fin->Get(proc+"/h_pdf_"+proc+"_syst"+TString(Form("%d",isyst))+"Up");    
       
-      h_syst_i = (TH2D*)h_syst_in_input->Clone(proc+"_"+TString(Form("%d", isyst)));
-      //TH2D* h_systDown_i = (TH2D*)fin->Get(proc+"/h_pdf_"+proc+"_systDown"+TString(Form("%d",isyst)));
+      h_syst_i        = (TH2D*)h_syst_in_input->Clone(proc+"_"+TString(Form("%d", isyst)));
+      h_systenvUp_i   = (TH2D*)h_syst_in_input->Clone("h_pdf_"+proc+"_syst"+TString(Form("%d",isyst))+"_scaleUp");
+      h_systenvDown_i = (TH2D*)h_syst_in_input->Clone("h_pdf_"+proc+"_syst"+TString(Form("%d",isyst))+"_scaleDown");
+      double envelope = 0.;
 
-      // loop over scales
+      // loop over scales	
       for( unsigned int iscale=0; iscale<scales.size(); iscale++){
-	cout << "Scale " << iscale << endl;
+	cout << "\t\tScale " << iscale << endl;
 	TString scale_name = scales[iscale].first;
 	int scale_idx  = scales[iscale].second;
 	TFile* fin_iscale = TFile::Open("root/fout_fit_scale_"+proc+"_"+phase_space+"_"+opt+"_"+proc+"_x1_y2.root", "READ");
@@ -76,7 +86,7 @@
 	int ibin_y = h_data_nom->GetYaxis()->FindBin(node_y);
 	//h_data_nom->Print("all");
 	//cout << "Bin:" << ibin << endl;
-	cout << h_data_iscale->GetBinContent( ibin_x, ibin_y ) << " / " << h_data_nom->GetBinContent( ibin_x, ibin_y ) << endl;	
+	//cout << "\t\tInputs:" << h_data_iscale->GetBinContent( ibin_x, ibin_y ) << " / " << h_data_nom->GetBinContent( ibin_x, ibin_y ) << endl;	
 	double binned_ratio =
 	  h_data_iscale->GetBinContent( ibin_x, ibin_y ) /
 	  h_data_nom->GetBinContent( ibin_x, ibin_y );
@@ -85,12 +95,33 @@
 	  avgy_binned_ratio += h_data_iscale->GetBinContent( ibin_x, k ) / h_data_nom->GetBinContent( ibin_x, k );	  
 	}
 	avgy_binned_ratio /= h_data_nom->GetYaxis()->GetNbins();
-	cout << "binned_ratio at the y node: " << binned_ratio << ", avg = " << avgy_binned_ratio << endl;
+	cout << "\t\tbinned_ratio at the y node: " << binned_ratio << ", avg = " << avgy_binned_ratio << endl;
+
+	// take the average
 	binned_ratio = avgy_binned_ratio;
-	
 	double rescale = (binned_ratio-1.0)/shift;
-	cout << "rescale by " << (binned_ratio-1.0) << "/" << shift << " = " << rescale << endl;
-	
+	cout << "\t\tRescale by (" << binned_ratio << " - 1.0)" << "/" << shift << " = " << rescale << endl;
+
+	if(TMath::Abs(binned_ratio - 1)>envelope){
+ 	  for(unsigned int ix=1; ix<=h_systenvUp_i->GetNbinsX(); ix++){
+	    if(h_systenvUp_i->GetXaxis()->GetBinUpEdge(ix)>xf_max) continue;
+	    for(unsigned int iy=1; iy<=h_systenvUp_i->GetNbinsY(); iy++){
+	      if(h_systenvUp_i->GetYaxis()->GetBinUpEdge(iy)>yf_max) continue;
+	      double nom_val = h_pdf->GetBinContent(ix,iy);
+	      double old_val = h_syst_i->GetBinContent(ix,iy);
+	      double corr = old_val>0. ? nom_val/old_val - 1.0 : 0.;
+	      corr *= rescale;
+	      h_systenvUp_i->SetBinContent(ix,iy, nom_val*(1+corr));
+	      h_systenvDown_i->SetBinContent(ix,iy, nom_val*(1-corr));
+	    }
+	  }	  
+	  envelope = TMath::Abs(binned_ratio - 1);
+	  cout << "\t\tsyst " << isyst << ": new envelope is |" << binned_ratio  << " - 1.0| = " << envelope << endl;  
+	}
+	else{
+	  cout << "\t\tsyst " << isyst << ": skipe because " << TMath::Abs(binned_ratio - 1) << "<=" << envelope << endl;
+	}
+
 	//fout->cd(proc);
 	TH2D* h_new_syst_i = (TH2D*)h_syst_i->Clone("h_pdf_"+proc+"_syst"+TString(Form("%d",isyst))+"_"+scales_names[iscale]);
 	for(unsigned int ix=1; ix<=h_new_syst_i->GetNbinsX(); ix++){
@@ -111,11 +142,16 @@
       }
     }
     
+    fout->cd(proc);
+    h_systenvUp_i->Write();
+    h_systenvDown_i->Write();          
+    
   }
 
 
   fin->Close();
   fout->Close();
+
   return;
 
 }
