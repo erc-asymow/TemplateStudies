93f35a0a7e491f78ebc02fc9cb829a240a745734
Lorenzo <lore.bianchini@gmail.com>
2025-06-06 16:15:59 +0200

    addee

--- plot_mcstat.py
+++ plot_mcstat.py
@@ -7,12 +7,13 @@ from sys import argv
 #argv.remove( '-b-' )
 import math
 import argparse
+import numpy as np
 
 parser = argparse.ArgumentParser(description='run')
 
 parser.add_argument('--none', action='store_true'  , help = 'none')
 parser.add_argument('--dryrun', action='store_true'  , help = 'dry run')
-parser.add_argument('-f', '--file', default='1_20_0p015_decorr'  , help = '')
+parser.add_argument('-f', '--file', default='1_200_0p015_corr'  , help = '')
 
 args = parser.parse_args()
 
@@ -50,8 +51,8 @@ def plot_tstat(fname = "1_200_0p015_decorr"):
     chi2.SetLineStyle(ROOT.kDashed)
     leg1.AddEntry(chi2, '#chi^{2}_{1}', 'L')
     leg1.Draw()
-    c.SaveAs('tstat.png')
-    c.SaveAs('tstat.pdf')
+    #c.SaveAs('tstat.png')
+    #c.SaveAs('tstat.pdf')
     input()
 
 def plot_muerr(var = "mu",  fname = "1_200_0p015_decorr"):
@@ -107,7 +108,7 @@ def plot_model(fname = "", eps = 0.03, N = 2e+06, k = 1, nbins = 200):
     leg1.SetNColumns(2) 
     xmin = 0.
     xmax = 1.
-    h1 = ROOT.TH1D("h1", "; bin number; ", nbins,0, nbins)
+    h1 = ROOT.TH1D("h1", "; bin index #font[12]{i}; ", nbins,0, nbins)
     h1.GetYaxis().SetNdivisions(5)
     h1.GetXaxis().SetNdivisions(5)
     h2 = ROOT.TH1D("h2", " ", nbins,0, nbins)
@@ -146,17 +147,113 @@ def plot_model(fname = "", eps = 0.03, N = 2e+06, k = 1, nbins = 200):
     h2.Draw("HISTSAME")
     h1t.Draw("HISTSAME")
     h2t.Draw("HISTSAME")
-    leg1.AddEntry(h1, '#nu_{1i}', 'L')
     leg1.AddEntry(h1t, 'T_{1i}', 'LE')
-    leg1.AddEntry(h2, '#nu_{2i}', 'L')
+    leg1.AddEntry(h1, '#nu_{1i}', 'L')
     leg1.AddEntry(h2t, 'T_{2i}', 'LE')
+    leg1.AddEntry(h2, '#nu_{2i}', 'L')
     leg1.Draw()
     #c.SaveAs('model.png')
     c.SaveAs(fname+'.pdf')
     #input()
 
+    
+def plot_simplemodel(fname = "simplemodel", var = 0.01):
 
+    ran = ROOT.TRandom3();
+    c = ROOT.TCanvas("c", "canvas", 600, 600)
+    #c.SetLogy(1)
+    leg1 = ROOT.TLegend(0.15,0.65, 0.55, 0.85, "","brNDC")
+    leg1.SetFillStyle(0)
+    leg1.SetBorderSize(0)
+    leg1.SetTextSize(0.04)
+    leg1.SetFillColor(10)
+    #leg1.SetNColumns(2)
+
+    nx = 50
+
+    rho2s = np.zeros( nx, dtype = float)
+    res1   = np.zeros( nx, dtype = float)
+    res2   = np.zeros( nx, dtype = float)
+    res3   = np.zeros( nx, dtype = float)
+
+    for i in range(nx):
+        print(i)
+        rho2 = float(i)/nx
+        rho2s[i] = rho2
+        sin = math.sqrt(rho2)
+        cos = math.sqrt(1-rho2)
+        Si1 = 0.
+        Si2 = 0.
+        Si3 = 0.
+        ntoys = 10000
+        Sitrue = (cos*cos)
+        for itoy in range(ntoys):
+            alpha1 = ran.Gaus(0., var)
+            alpha2 = ran.Gaus(0., var)
+            Siitoy1 = (1 - (sin+alpha1)*(sin+alpha1)/( (sin+alpha1)*(sin+alpha1) + (cos+alpha2)*(cos+alpha2) ))
+            Si1 += Siitoy1
+            alpha1 *= 2
+            alpha2 *= 2
+            Siitoy2 = (1 - (sin+alpha1)*(sin+alpha1)/( (sin+alpha1)*(sin+alpha1) + (cos+alpha2)*(cos+alpha2) ))
+            Si2 += Siitoy2
+            alpha1 *= 2
+            alpha2 *= 2
+            Siitoy3 = (1 - (sin+alpha1)*(sin+alpha1)/( (sin+alpha1)*(sin+alpha1) + (cos+alpha2)*(cos+alpha2) ))
+            Si3 += Siitoy3            
+            #if i==0:
+            #    print( Siitoy )
+            #    print("....", Sitrue)
+        Si1 /= ntoys
+        Si2 /= ntoys
+        Si3 /= ntoys
+        res1[i] = (Si1/Sitrue)
+        res2[i] = (Si2/Sitrue)
+        res3[i] = (Si3/Sitrue)
+
+    #print(rho2s)
+    #print(res)
+    gr1 = ROOT.TGraph( nx , rho2s, res1)
+    gr2 = ROOT.TGraph( nx , rho2s, res2)
+    gr3 = ROOT.TGraph( nx , rho2s, res3)
+    mg = ROOT.TMultiGraph()
+    mg.GetYaxis().SetRangeUser(0.98,1.10)
+    mg.GetXaxis().SetRangeUser(0.,1.)
+    mg.GetYaxis().SetTitle( '<#hat{S}>/S' )
+    mg.GetXaxis().SetTitle( '#rho^{2}_{#mu}' )
+    mg.GetYaxis().SetTitleOffset(0.71)
+    mg.GetXaxis().SetTitleOffset(0.71)
+    mg.GetYaxis().SetTitleSize(0.06)
+    mg.GetXaxis().SetTitleSize(0.06)
+    gr1.SetMarkerStyle(ROOT.kFullCircle)
+    gr1.SetMarkerSize(1.)
+    gr1.SetMarkerColor(ROOT.kRed)
+    gr2.SetMarkerStyle(ROOT.kFullCircle)
+    gr2.SetMarkerSize(1.)
+    gr2.SetMarkerColor(ROOT.kBlue)
+    gr3.SetMarkerStyle(ROOT.kFullCircle)
+    gr3.SetMarkerSize(1.)
+    gr3.SetMarkerColor(ROOT.kOrange)
+    mg.Add(gr1)
+    mg.Add(gr2)
+    mg.Add(gr3)
+    mg.Draw( 'APL' )
+    leg1.SetHeader("#sqrt{<#alpha^{2}>}")
+    leg1.AddEntry(gr1, "%.2f" % (var), "LP")
+    leg1.AddEntry(gr2, "%.2f" % (var*2), "LP")
+    leg1.AddEntry(gr3, "%.2f" % (var*4), "LP")
+    line = ROOT.TF1('line','1',0, 1.)
+    line.SetLineWidth(3)
+    line.SetLineStyle(ROOT.kDashed)
+    line.SetLineColor(ROOT.kBlack)
+    line.Draw("same")
+    leg1.Draw()
+    c.Update()        
+    #c.SaveAs(fname+'.pdf')
+    input()
+
+    
 if __name__ == '__main__':
     #plot_tstat( fname=args.file )
-    plot_model( fname="model_k10", k=10)
+    #plot_model( fname="model_k10", k=10)
     #plot_muerr( var = "mu", fname=args.file)
+    plot_simplemodel("simplemodel", 0.03)
